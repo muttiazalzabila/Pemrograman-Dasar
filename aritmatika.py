c8c863b0e1c9d2f439c955eaaac0cc2d7c1acf06
muttiazalzabila <muttia04051982@gmail.com>
2025-11-08 11:10:17 +0800

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,24 @@
+A = 20
+B = 15
+print (A - B)
+print (A + B)
+print (A * B)
+print (A / B)
+
+#penjumlahan
+n = 20
+r = 15
+penjumlahan = n + r
+print("Hasil penjumlahan:", penjumlahan)
+
+#pengurangan
+pengurangan = n - r
+print("Hasil pengurangan", pengurangan)
+
+#perkalian
+perkalian = n * r
+print ("Hasil perkalian", perkalian)
+
+#pembagian
+pembagian = n / r
+print("Hasil pembagian", pembagian)
