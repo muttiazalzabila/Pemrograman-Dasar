0087d3404d6bc05fd060936eafb65b25b961b3c4
muttiazalzabila <muttia04051982@gmail.com>
2025-12-11 15:10:30 +0800

    Update and rename algorita binary search dan interpolation search.py to algoritma binary search dan interpolation search.py

---
+++ final test/algoritma binary search dan interpolation search.py
@@ -0,0 +1,23 @@
+binary search
+
+def binary_search(arr, x):
+ low = 0
+ high = len(arr)-1
+ while low <= high:
+ mid = (low+high)//2
+ if arr[mid] == x: return mid
+ elif arr[mid] < x: low = mid+1
+ else: high = mid-1
+ return -1
+
+Interpolation Search
+
+def interpolation_search(arr, x):
+ low = 0
+ high = len(arr)-1
+ while low <= high and x >= arr[low] and x <= arr[high]:
+ pos = low + ((x-arr[low])*(high-low)//(arr[high]-arr[low]))
+ if arr[pos] == x: return pos
+ elif arr[pos] < x: low = pos+1
+ else: high = pos-1
+ return -1
