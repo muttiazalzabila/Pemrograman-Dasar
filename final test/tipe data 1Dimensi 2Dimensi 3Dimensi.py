30b7491abf72154ec28250587d4d64ba78edd844
muttiazalzabila <muttia04051982@gmail.com>
2025-12-11 13:42:02 +0700

    Update and rename 1.2 array 1d, 2d, 3d.py to tipe data 1Dimensi 2Dimensi 3Dimensi.py

--- final test/tipe data 1Dimensi 2Dimensi 3Dimensi.py
+++ final test/tipe data 1Dimensi 2Dimensi 3Dimensi.py
@@ -4,9 +4,6 @@
 array_1d = [10, 20, 30, 40, 50]
 print("Array 1 Dimensi:")
 print(array_1d)
-print("-------------------------")
-
-
 # ------------------------------------
 # ARRAY / LIST 2 DIMENSI
 # (Array dalam array)
@@ -20,8 +17,6 @@ print("Array 2 Dimensi:")
 for baris in array_2d:
     print(baris)
 print("-------------------------")
-
-
 # ------------------------------------
 # ARRAY / LIST 3 DIMENSI
 # (Array 2D di dalam array)
@@ -41,5 +36,3 @@ print("Array 3 Dimensi:")
 for layer in array_3d:
     for baris in layer:
         print(baris)
-    print("----- layer selesai -----")
-
