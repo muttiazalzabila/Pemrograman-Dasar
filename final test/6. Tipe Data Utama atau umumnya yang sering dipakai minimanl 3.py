3560b7f16799a6e99ee292f6afb12beb035e35f8
muttiazalzabila <muttia04051982@gmail.com>
2025-12-11 20:23:13 +0800

    6. Tipe Data Utama atau umumnya yang sering dipakai minimanl 3.py

---
+++ final test/6. Tipe Data Utama atau umumnya yang sering dipakai minimanl 3.py
@@ -0,0 +1,24 @@
+# =========================================
+#   Tipe Data Utama
+#   Minimal 3 Tipe Data
+#   By: Muttia Zalzabila.S
+# =========================================
+
+# 1. Integer → angka bulat
+jumlah_siswa = 30
+
+# 2. Float → angka desimal
+nilai_rata_rata = 85.75
+
+# 3. String → teks
+nama_kelas = "Sistem Informasi A"
+
+print("=== TIPE DATA UTAMA ===")
+print("Integer :", jumlah_siswa)
+print("Float   :", nilai_rata_rata)
+print("String  :", nama_kelas)
+
+print("\n=== CEK TIPE DATA ===")
+print("Tipe jumlah_siswa   =", type(jumlah_siswa))
+print("Tipe nilai_rata_rata =", type(nilai_rata_rata))
+print("Tipe nama_kelas      =", type(nama_kelas))
